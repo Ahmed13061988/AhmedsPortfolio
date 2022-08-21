af4422d3fe639cfbfc5c3fb9e89507994145a595
Ahmed Hussein <wolktheline@yahoo.com>
2022-08-21 14:40:02 -0600

    adding overlay property

--- script.js
+++ script.js
@@ -3,13 +3,16 @@
 const btnJobEl = document.querySelector(".btn--job");
 const btnClose = document.querySelector(".close-job");
 const jobDescription = document.querySelector(".job");
+const overlay = document.querySelector(".overlay");
 
 btnJobEl.addEventListener("click", function () {
   jobDescription.classList.remove("hidden");
+  overlay.classList.remove("hidden");
 });
 
 btnClose.addEventListener("click", function () {
   jobDescription.classList.add("hidden");
+  overlay.classList.add("hidden");
 });
 
 document.addEventListener("keydown", function (e) {
