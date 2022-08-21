58e5fb8a9dea33ec06c0568831c99c3ad52eb887
Ahmed Hussein <wolktheline@yahoo.com>
2022-08-21 14:33:43 -0600

    adding button to show my experience

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 "use strict";
+
+const btnJobEl = document.querySelector(".btn--job");
+const btnClose = document.querySelector(".close-job");
+const jobDescription = document.querySelector(".job");
+
+btnJobEl.addEventListener("click", function () {
+  jobDescription.classList.remove("hidden");
+});
+
+btnClose.addEventListener("click", function () {
+  jobDescription.classList.add("hidden");
+});
+
+document.addEventListener("keydown", function (e) {
+  if (e.key === "Escape") jobDescription.classList.add("hidden");
+});
