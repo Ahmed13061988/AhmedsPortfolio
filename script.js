bce399373294773144bc0485f9741ad1be2a24b5
Ahmed Hussein <wolktheline@yahoo.com>
2022-08-22 11:53:05 -0600

    adding the close button to the hobbies

--- script.js
+++ script.js
@@ -1,35 +1,47 @@
 "use strict";
 
 const btnJobEl = document.querySelector(".btn--job");
-const btnClose = document.querySelector(".close-job");
-const btnHobbies = document,querySelector('.btn--hobbies');
+const btnCloseJob = document.querySelector(".close-job");
+const btnCloseHobbies = document.querySelector(".close-hobbies");
+const btnHobbies = document.querySelector(".btn--hobbies");
 const jobDescription = document.querySelector(".job");
-const hobbies = document.querySelector('.hobbies');
+const hobbies = document.querySelector(".hobbies");
 const overlay = document.querySelector(".overlay");
 
-const closing = function () {
-  jobDescription.classList.add("hidden");
-  overlay.classList.add("hidden");
-};
-
+// const closing = function () {
+//   jobDescription.classList.add("hidden");
+//   overlay.classList.add("hidden");
+// };
 
 btnJobEl.addEventListener("click", function () {
   jobDescription.classList.remove("hidden");
   overlay.classList.remove("hidden");
 });
 
-btnHobbies.addEventListener('click', function(){
-    hobbies.classList.remove("hidden");
-    overlay.classList.remove("hidden");
-})
+btnHobbies.addEventListener("click", function () {
+  hobbies.classList.remove("hidden");
+  overlay.classList.remove("hidden");
+});
 
-btnClose.addEventListener("click", closing);
+btnCloseJob.addEventListener("click", function () {
+  jobDescription.classList.add("hidden");
+  overlay.classList.add("hidden");
+});
+btnCloseHobbies.addEventListener("click", function () {
+  hobbies.classList.add("hidden");
+  overlay.classList.add("hidden");
+});
 
-overlay.addEventListener("click", closing);
+overlay.addEventListener("click", function () {
+  hobbies.classList.add("hidden");
+  jobDescription.classList.add("hidden");
+  overlay.classList.add("hidden");
+});
 
 document.addEventListener("keydown", function (e) {
   if (e.key === "Escape") {
     jobDescription.classList.add("hidden");
+    hobbies.classList.add("hidden");
     overlay.classList.add("hidden");
   }
 });
